1c46cd46883c03aea1f265d9e65a78d88d9e4083
Ben Green <ben@latenightsketches.com>
2019-07-05 13:32:14 -0700

    Store parsed template code as privately-accessible sybmol key

--- Template.js
+++ Template.js
@@ -4,10 +4,13 @@ const fs = require('fs');
 const TAG_START = '<%';
 const TAG_END = '%>';
 
+// Store parsed template code using privately-accessible symbol key
+const PARSED = Symbol('parsed');
+
 module.exports = class Template {
   constructor(filename) {
     const raw = fs.readFileSync(filename, 'utf8').split(TAG_START);
-    this.parsed = 'let __out=`' + backtickEscape(raw[0]) + '`;';
+    this[PARSED] = 'let __out=`' + backtickEscape(raw[0]) + '`;';
 
     for(let i = 1; i < raw.length; i++) {
       const tagEnd = raw[i].indexOf(TAG_END);
@@ -19,20 +22,20 @@ module.exports = class Template {
       const after = raw[i].substr(tagEnd + TAG_END.length);
 
       if(expr.substr(0,1) === '=') {
-        this.parsed += '__out+=__escape(' + expr.substr(1) + ');';
+        this[PARSED] += '__out+=__escape(' + expr.substr(1) + ');';
       } else {
-        this.parsed += expr;
+        this[PARSED] += expr;
       }
-      this.parsed += '__out+=`' + backtickEscape(after) + '`;';
+      this[PARSED] += '__out+=`' + backtickEscape(after) + '`;';
     }
-    this.parsed += 'return __out;';
+    this[PARSED] += 'return __out;';
   }
   render(ctx) {
     // Although this looks like it would be slower than building this function
     // in the constructor with predefined context keys, the Javascript engine
     // caches the result so that subsequent calls are just as fast as if the
     // function was created and stored for later use
-    return (new Function(...Object.keys(ctx), '__escape', this.parsed))(...Object.values(ctx), htmlEscape);
+    return (new Function(...Object.keys(ctx), '__escape', this[PARSED]))(...Object.values(ctx), htmlEscape);
   }
 }
 
