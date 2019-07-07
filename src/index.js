9bd515fb2ec7a9a62f26ee2f498e04c4c6290651
Ben Green <ben@latenightsketches.com>
2019-07-06 17:29:15 -0700

    Do not wait for request before fetching initial data

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 const BeachReportServer = require('./BeachReportServer');
 const BeachReportData = require('./BeachReportData');
 
-const app = new BeachReportServer(
-  // Refresh data every 3 hours or specified duration
-  new BeachReportData(process.env.REFRESH_INTERVAL || 3*60*60*1000),
-  process.env.ENFORCE_HTTPS,
-);
+// Refresh data every 3 hours or specified duration
+const provider = new BeachReportData(process.env.REFRESH_INTERVAL || 3*60*60*1000);
+const app = new BeachReportServer(provider, process.env.ENFORCE_HTTPS);
 
 app.on('error', error => console.error(error));
 
+provider.fetch() // Do not wait for a request before loading initial data
 app.listen(process.env.PORT || 3000);
